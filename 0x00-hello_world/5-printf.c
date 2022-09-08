6e29bd1fc08b6baeb4be87ae14b2a9250839f908
Jobelynz <davidadinnu@gmail.com>
2022-09-08 11:06:09 -0700

    Low level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry program
+ * description: this is the starting point
+ * Return: (0) success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
