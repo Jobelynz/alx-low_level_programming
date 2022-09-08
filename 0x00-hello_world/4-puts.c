a66379e314a12168be49212e7f7c3695a007d5be
Jobelynz <davidadinnu@gmail.com>
2022-09-08 10:10:23 -0700

    Low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - the main function
+* char - character
+* return - return (0) after success
+*/
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return (0);
+}
