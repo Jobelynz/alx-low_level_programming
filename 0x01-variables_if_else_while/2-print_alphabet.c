f39d25b02711e09314a67e517a823c5ec74763a3
Jobelynz <davidadinnu@gmail.com>
2022-09-09 08:53:59 -0700

    Low level programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - the start of the code
+*Return: (0) success
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}       
+	putchar('\n');
+	return (0);
+}
