a987ab3bdde14956259a573310f9e5edfe5d5fb6
Jobelynz <davidadinnu@gmail.com>
2022-09-13 08:13:59 -0700

    C functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,12 @@
 #include "main.h"
 #include "_putchar.c"
+#include <stdio.h>
 /**
 * main - Entry point of the program
 * Return: (0) success
+* ('_') - beginnig of putchar
 **/
+int main(void)
 {
 	putchar('_');
 	putchar('p');
