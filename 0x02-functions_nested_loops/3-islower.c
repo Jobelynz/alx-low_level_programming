9fd57a1d08e66193d71ec93de4ad8fe539c3864d
Jobelynz <davidadinnu@gmail.com>
2022-09-13 11:08:02 -0700

    C functions and nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,13 +6,13 @@
 */
 int _islower(int c)
 {
-	if (c != 97 || c != 122)
+	if (c >= 'a' && c <= 'z')
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 	_putchar('\n');
 	return (0);
