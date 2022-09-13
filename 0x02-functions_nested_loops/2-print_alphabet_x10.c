3e6699a2d155f1bfcd413dc6ca26fff5ac7b98fd
Jobelynz <davidadinnu@gmail.com>
2022-09-13 09:37:25 -0700

    C functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints the alphabet 10x
+*/
+void print_alphabet_x10(void)
+{
+	char alpha;
+
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
