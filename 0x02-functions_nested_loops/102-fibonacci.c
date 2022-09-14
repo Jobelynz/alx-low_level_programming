d1186962d47bc36e1781c13733548041c1ef9fde
Jobelynz <davidadinnu@gmail.com>
2022-09-13 21:02:26 -0700

    C functions and nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - entry point
+* Return: (0) success
+*/
+int main(void)
+{
+	int num;
+
+	unsigned long fib1 = 0, fib2 = 1, add;
+
+	for (num = 0; num < 50, num++)
+	{
+		add = fib1 + fib2;
+		printf("%lu", add);
+		fib1 = fib2;
+		fib2 = add;
+		if (num == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(",");
+		}
+	}
+	return (0);
+}
