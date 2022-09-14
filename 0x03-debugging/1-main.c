b99209b64fe5a2bc1835d284a05c3b5f0ce7c22b
Jobelynz <davidadinnu@gmail.com>
2022-09-14 19:31:17 +0100

    Debugging in C

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,10 +10,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 /**
-*	while (i < 10)
+* while - comment
+* while (i < 10)
 *	{
 *		putchar(i);
 *	}
+* @\\o/\n": returns \\o/ on a newline
+* Description: reurns \\o/ on a newline
 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
