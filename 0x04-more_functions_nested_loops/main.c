880b91362087ec675edff73701dcfac8b452f58f
Jobelynz <davidadinnu@gmail.com>
2022-09-15 11:16:13 -0700

    More functions and nested loops

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -6,34 +6,3 @@
  */
 #include <stdio.h>
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int _isdigit(int c);
-
-int main(void)
-{
-    char c;
-
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = '9';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
-}
-
-int _isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	return (0);
-}
