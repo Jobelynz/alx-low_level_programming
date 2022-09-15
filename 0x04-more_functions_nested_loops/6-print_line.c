4052dbd9a2d79f05d4fe29ec7423bf538198da8c
Jobelynz <davidadinnu@gmail.com>
2022-09-15 14:12:11 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_line - draws a straight based on n
+* @n: argument
+*/
+void print_line(int n)
+{
+	int z;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (z = 0; z < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
