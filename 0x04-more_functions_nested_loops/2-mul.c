39f7d34b47792755539d8d1eb9ae874ca5679b2f
Jobelynz <davidadinnu@gmail.com>
2022-09-15 11:22:04 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @a: first argument
+* @b: second argument
+* Return: 0 Always
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -5,4 +5,22 @@
  * @c: argument
  */
 #include <stdio.h>
+int mul(int a, int b);
+int main(void)
+{
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return (0);
+}
 
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @a: first argument
+* @b: second argument
+* Return: 0 Always
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
