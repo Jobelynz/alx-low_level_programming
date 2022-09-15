b487f96ad8c35a215013980edad31564c7d060bd
Jobelynz <davidadinnu@gmail.com>
2022-09-15 16:33:10 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - The main function
+* Return: Always 0
+*/
+int main(void)
+{
+	long int prime = 612852475143;
+
+	long int prime_num = (prime / 12057);
+
+	printf("%ld", prime_num);
+	printf("\n");
+	return (0);
+}
