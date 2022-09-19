1ce39131ec2e4a605e1ed77bc634b3371e78f65f
Jobelynz <davidadinnu@gmail.com>
2022-09-19 11:57:36 -0700

    Pointers, arrays and strings in C

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* puts_half - prints half of a string
+* @str: pointer parameter
+*/
+void puts_half(char *str)
+{
+	int x, y;
+
+	x = 0;
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	if (x % 2 == 1)
+	{
+		y = (x - 1) / 2;
+		y += 1;
+	}
+	else
+	{
+		y = x / 2;
+	}
+	for (; y < x; y++)
+	{
+		_putchar(str[y]);
+	}
+	_putchar('\n');
+}
