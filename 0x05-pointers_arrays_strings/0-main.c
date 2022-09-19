c69233e62b1cd1f162d066830993fb7b7f6f654d
Jobelynz <davidadinnu@gmail.com>
2022-09-19 13:26:30 -0700

    Pointers, arrays and strings in C

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,16 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
+ * main - check the code for
  *
  * Return: Always 0.
  */
-void puts_half(char *str);
+void print_array(int *a, int n);
 int main(void)
 {
-    char *str;
+    int array[5];
 
-    str = "0123456789";
-    puts_half(str);
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
     return (0);
 }
