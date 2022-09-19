bfff5b763761640c0fe9fc80ade5a6cf75002cee
Jobelynz <davidadinnu@gmail.com>
2022-09-19 09:38:55 -0700

    Pointers, arrays and strings in C

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -5,16 +5,14 @@
  *
  * Return: Always 0.
  */
-void swap_int(int *a, int *b);
+int _strlen(char *s);
 int main(void)
 {
-    int a;
-    int b;
+    char *str;
+    int len;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
     return (0);
 }
