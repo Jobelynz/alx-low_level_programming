f51e946f0bf1846ed42d48f01f17d0522d461c3d
Jobelynz <davidadinnu@gmail.com>
2022-09-22 14:11:05 -0700

    More pointers, arrays and strings in C

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,41 +18,25 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
-			{
 				printf("  ");
-			}
 		}
 			else
-			{
 				printf("%02x", *(b + index + byte));
-			}
 			if ((index % 2) != 0 && index != 0)
-			{
 				printf(" ");
-			}
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
-			{
 				break;
-			}
 			else if (*(b + index + byte) >= 31 &&
 				 *(b + index + byte) <= 126)
-			{
 				printf("%c", *(b + index + byte));
-			}
 			else
-			{
 				printf(".");
-			}
 		}
 		if (byte >= size)
-		{
-		continue;
-		}
+			continue;
 		printf("\n");
 	if (size <= 0)
-	{
 		printf("\n");
-	}
 }
