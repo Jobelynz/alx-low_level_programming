8443cef861575b04f22d477b107ec9e40190ad52
Jobelynz <davidadinnu@gmail.com>
2022-09-23 09:27:40 -0700

    More pointers, arrays and and strings in C

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,34 +9,37 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int byte, index;
+	int i, j, k, l, m, n;
 
-	for (byte = 0; byte < size; byte += 10)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		printf("%08x: ", byte);
-		for (index = 0; index < 10; index++)
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
 		{
-			if ((index + byte) >= size)
-				printf("  ");
-			else
-				printf("%02x", *(b + index + byte));
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
+			break;
 		}
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
-			else if (*(b + index + byte) >= 31 &&
-				 *(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-			else
-				printf(".");
-		}
-		if (byte >= size)
-			continue;
-		printf("\n");
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-	if (size <= 0)
-		printf("\n");
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* print_buffer -  a function that prints a buffer.
+* @b: buffer
+* @size: size of the buffer
+* Return: 0
+*/
+void print_buffer(char *b, int size)
+{
+
