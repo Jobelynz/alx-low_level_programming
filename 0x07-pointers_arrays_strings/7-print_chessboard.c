10c408c36aaf138f22688f2cc8c723915bd4051f
Jobelynz <davidadinnu@gmail.com>
2022-09-26 14:16:55 -0700

    Even more pointers, arrays and strings in C

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,12 @@ void print_chessboard(char (*a)[8])
 {
 	int x, y;
 
-	for (x = 0, x < 8; x++)
+	for (x = 0; x < 8; x++)
 	{
-		for (y = 0; j < 8; j++)
+		for (y = 0; y < 8; y++)
 		{
-			putchar(a[x][y]);
+			_putchar(a[x][y]);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
